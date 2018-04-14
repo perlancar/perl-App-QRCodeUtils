91a8ca52717572fe16b42164281b5097af28b290
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-04-14 08:52:05 +0700

    Initial commit

---
+++ lib/App/QRCodeUtils.pm
@@ -0,0 +1,49 @@
+package App::QRCodeUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+$SPEC{decode_qrcode} = {
+    v => 1.1,
+    summary => 'Decode QR Code',
+    args => {
+        filename => {
+            schema => 'filename*',
+            req => 1,
+            pos => 0,
+        },
+    },
+    examples => [
+    ],
+};
+sub decode_qrcode {
+    require Image::DecodeQR;
+
+    my %args = @_;
+
+    my $str = Image::DecodeQR::decode($args{filename});
+
+    [200, "OK", $str];
+}
+
+1;
+#ABSTRACT: Utilities related to QR Code
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<App::GoogleAuthUtils>
+
+=cut
