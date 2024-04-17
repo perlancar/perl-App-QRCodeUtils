bcbbfca01355a6d337c2b37b994db0961f1f51da
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-04-17 16:12:56 +0700

    Add utility: gen-qrcode

--- lib/App/QRCodeUtils.pm
+++ lib/App/QRCodeUtils.pm
@@ -1,11 +1,13 @@
 package App::QRCodeUtils;
 
-# DATE
-# VERSION
-
 use 5.010001;
 use strict;
 use warnings;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
 
 our %SPEC;
 
@@ -32,6 +34,58 @@ sub decode_qrcode {
     my $str = Image::DecodeQR::decode($args{filename});
 
     [200, "OK", $str];
+}
+
+$SPEC{gen_qrcode} = {
+    v => 1.1,
+    summary => 'Generate QR Code and by default show it (or save it to a file)',
+    args => {
+        text => {
+            schema => 'str*',
+            req => 1,
+            pos => 0,
+        },
+        filename => {
+            schema => 'filename*',
+            pos => 1,
+            description => <<'MARKDOWN',
+
+If unspecified, will save to a temporary filename and show it with
+<pm:Desktop::Open>.
+
+MARKDOWN
+        },
+        format => {
+            schema => ['str*', in=>[qw/png html txt/]],
+            default => 'png',
+        },
+    },
+    examples => [
+    ],
+};
+sub gen_qrcode {
+    require QRCode::Any;
+
+    my %args = @_;
+    my $format = $args{format};
+
+    my $filename = $args{filename};
+    unless (defined $filename) {
+        require File::Temp;
+        (undef, $filename) = File::Temp::tempfile("qrcodeXXXXXXXXX", SUFFIX=>".$format");
+    }
+
+    my $res = QRCode::Any::encode_qrcode(
+        format => $format,
+        text => $args{text},
+        filename => $filename,
+    );
+    return $res unless $res->[0] == 200;
+
+    require Desktop::Open;
+    Desktop::Open::open_desktop($filename);
+
+    [200];
 }
 
 1;
